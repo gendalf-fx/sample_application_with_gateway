bc432e72f0deecd3452eeb04bc846342ebe5f5bb
Artem <puhachofficial@gmail.com>
2022-05-20 14:04:01 +0300

    Created the structure of the project

---
+++ src/main/java/com/gendalf/gendalfgateway/GendalfGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gendalf.gendalfgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GendalfGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GendalfGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gendalf/gendalfgateway/GendalfGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gendalf.gendalfgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GendalfGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
