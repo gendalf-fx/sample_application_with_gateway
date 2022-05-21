1bf6d03f4b3336e981d8c9489dbcdc04bcbdda85
Artem <puhachofficial@gmail.com>
2022-05-21 14:13:22 +0300

    Changed gateway configurations from yaml file to Java anotation format

--- gateway/src/main/java/com/gendalf/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gendalf/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.gendalf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(r -> r.method(HttpMethod.GET)
+                .uri("http://localhost:8081/reader/news"))
+            .build();
     }
 
 }
